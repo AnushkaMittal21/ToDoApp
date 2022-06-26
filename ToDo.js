59a2eb18ae838b8b5b65353a709f96983b5e96f6
AnushkaMittal21 <88485333+AnushkaMittal21@users.noreply.github.com>
2022-06-26 13:11:15 +0530

    Add files via upload

---
+++ ToDo.js
@@ -0,0 +1,49 @@
+// getting all required elements
+const inputBox = document.querySelector(".inputfield input");
+const addBtn = document.querySelector(".inputfield i");
+const todoList = document.querySelector(".todoList");
+const deleteAllBtn = document.querySelector(".footer button");
+
+showTasks(); //calling showTask function
+
+addBtn.onclick = () => { //when user click on plus icon button
+    let userEnteredValue = inputBox.value; //getting input field value
+    let getLocalStorageData = localStorage.getItem("New Todo"); //getting localstorage
+    if (getLocalStorageData == null) { //if localstorage has no data
+        listArray = []; //create a blank array
+    } else {
+        listArray = JSON.parse(getLocalStorageData);  //transforming json string into a js object
+    }
+    listArray.push(userEnteredValue); //pushing or adding new value in array
+    localStorage.setItem("New Todo", JSON.stringify(listArray)); //transforming js object into a json string
+    showTasks(); //calling showTask function
+}
+
+function showTasks() {
+    let getLocalStorageData = localStorage.getItem("New Todo");
+    if (getLocalStorageData == null) {
+        listArray = [];
+    } else {
+        listArray = JSON.parse(getLocalStorageData);
+    }
+    let newLiTag = "";
+    listArray.forEach((element, index) => {
+        newLiTag += `<li>${element}<span class="icon" onclick="deleteTask(${index})"><i class="fas fa-trash"></i></span></li>`;
+    });
+    todoList.innerHTML = newLiTag; //adding new li tag inside ul tag
+    inputBox.value = ""; //once task added leave the input field blank
+}
+// delete task function
+function deleteTask(index) {
+    let getLocalStorageData = localStorage.getItem("New Todo");
+    listArray = JSON.parse(getLocalStorageData);
+    listArray.splice(index, 1); //delete or remove the li
+    localStorage.setItem("New Todo", JSON.stringify(listArray));
+    showTasks(); //call the showTasks function
+}
+// delete all tasks function
+deleteAllBtn.onclick = () => {
+    listArray = []; //empty the array
+    localStorage.setItem("New Todo", JSON.stringify(listArray)); //set the item in localstorage
+    showTasks(); //call the showTasks function
+}
